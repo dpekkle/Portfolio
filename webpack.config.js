993d5157cc5742e1d55657ef27e694bc1653f45f
Danielle Cerisier <danielle.cerisier@gmail.com>
2017-01-18 14:03:56 +1100

    node_modules cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+//URL: http://localhost:8080/webpack-dev-server/#
+
 var webpack = require("webpack");
 var path = require("path");
 
@@ -10,26 +12,6 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = {
 	//entry: JSX + "/index.jsx",
-	// devServer: {
-	// 	//hack to work with react-router https://stackoverflow.com/questions/26203725/how-to-allow-for-webpack-dev-server-to-allow-entry-points-from-react-router
-	// 	proxy: { 
-	// 		'/**': {//catch all requests
-	// 			target: '/index.html',//default target
-	// 			secure: false,
-	// 			bypass: function(req, res, opt){
-	// 				//your custom code to check for any exceptions
-	// 				//console.log('bypass check', {req: req, res:res, opt: opt});
-	// 				if(req.path.indexOf('/img/') !== -1 || req.path.indexOf('/public/') !== -1){
-	// 					return '/'
-	// 				}
-
-	// 				if (req.headers.accept.indexOf('html') !== -1) {
-	// 					return '/index.html';
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// },
 	entry: [
 		'webpack-dev-server/client?http://localhost:8080',
 		//'webpack/hot/only-dev-server',
